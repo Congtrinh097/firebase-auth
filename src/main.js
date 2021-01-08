76741c731f38163c516563187130823d96ae13fe
trinhtc <congtrinh097@gmail.com>
2021-01-08 22:56:44 +0700

    Login and Regiter account

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from "firebase";
+import router from "./routes/index";
+import store from "./store";
 
 Vue.config.productionTip = false
 
+var firebaseConfig = {
+  apiKey: "AIzaSyCUAwb_NUEdtHfZgll8KBK5_PBQgqyuGpM",
+  authDomain: "myself-b566d.firebaseapp.com",
+  databaseURL: "https://myself-b566d.firebaseio.com",
+  projectId: "myself-b566d",
+  storageBucket: "myself-b566d.appspot.com",
+  messagingSenderId: "20072697511",
+  appId: "1:20072697511:web:6326db8215718c89b6d582"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
